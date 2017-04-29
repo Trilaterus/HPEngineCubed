0bedf024d744c0892e86d3c273ef871d398fac8d
HussainPatel <hussainpatel@hotmail.co.uk>
2017-04-29 16:33:33 +0100

    UIText rendering fixed
    TG-42 #Done - TG-43 #Done - Fixed the major text rendering issues. Basic
    features now work

--- NanDaiYoCode/TestOverlay.cpp
+++ NanDaiYoCode/TestOverlay.cpp
@@ -18,9 +18,21 @@ namespace Overlays
 		UIImage background = UIImage("background", m_Window, UIPosition());
 		m_AllUI.push_back(std::make_shared<UIImage>(background));
 
-		UIText centreAlign = UIText("Lin", m_Window, UIPosition(), UIText::Alignment::LEFT);
+		UIText centreAlign = UIText("Hemi", m_Window, UIPosition(), UIText::Alignment::CENTRE);
 		centreAlign.setText("Normal colour, <c 123 213 123>different colour, <c 1 2 3 150>colour<br>transparency,</> prev colour,<br></>normal colour");
 		m_AllUI.push_back(std::make_shared<UIText>(centreAlign));
+
+		UIText topLeft = UIText("Lin", m_Window, UIPosition(UIAnchor::TOP | UIAnchor::LEFT), UIText::Alignment::RIGHT);
+		topLeft.setText("This text is aligned properly even though one line<br>is way longer!");
+		m_AllUI.push_back(std::make_shared<UIText>(topLeft));
+
+		UIText botRight = UIText("M12 edited", m_Window, UIPosition(UIAnchor::BOT | UIAnchor::RIGHT), UIText::Alignment::CENTRE);
+		botRight.setText("<c 255 50 50>Even this is aligned!<br>And its colour is applied without an end tag");
+		m_AllUI.push_back(std::make_shared<UIText>(botRight));
+
+		UIText botLeft = UIText("Pixeltype", m_Window, UIPosition(UIAnchor::BOT | UIAnchor::LEFT), UIText::Alignment::CENTRE);
+		botLeft.setText("For some reason<br>this doesn't work?<br>Sad face :(");
+		m_AllUI.push_back(std::make_shared<UIText>(botLeft));
 	}
 
 	void Test::handleEvents(const sf::Event& event)
@@ -40,22 +52,5 @@ namespace Overlays
 	void Test::draw(sf::RenderTarget& target, sf::RenderStates states) const
 	{
 		Scene::draw(target, states);
-
-		sf::Vertex vline[] =
-		{
-			sf::Vertex(sf::Vector2f(target.getSize().x / 2.f, 0.f)),
-			sf::Vertex(sf::Vector2f(target.getSize().x / 2.f, static_cast<float>(target.getSize().y)))
-		};
-
-		target.draw(vline, 2, sf::Lines);
-
-		sf::Vertex hline[] =
-		{
-			sf::Vertex(sf::Vector2f(0.f, static_cast<float>(target.getSize().y / 2))),
-			sf::Vertex(sf::Vector2f(static_cast<float>(target.getSize().x), static_cast<float>(target.getSize().y / 2)))
-		};
-
-		target.draw(hline, 2, sf::Lines);
-
 	}
 }
--- NanDaiYoCode/UIText.cpp
+++ NanDaiYoCode/UIText.cpp
@@ -7,25 +7,10 @@ UIText::UIText(const std::string& sFontName, const sf::RenderTarget& target, UIP
 	:
 	UIObject(position),
 	m_sFontName(sFontName),
-	m_Alignment(align),
-	m_sfRenderTexture(new sf::RenderTexture())
+	m_Alignment(align)
 {
-	m_sfRenderTexture->setSmooth(true);
-
 	UIObject::setScreenAnchor(&m_sfTextSprite, target);
 	UIObject::setOffsetPosition(&m_sfTextSprite, position.m_fXOffset, position.m_fYOffset);
-}
-
-UIText::UIText(const UIText& uiText)
-	:
-	UIObject(uiText.m_Position),
-	m_sFontName(uiText.m_sFontName),
-	m_Alignment(uiText.m_Alignment),
-	m_sfRenderTexture(new sf::RenderTexture())
-{
-	m_sfRenderTexture->setSmooth(true);
-	m_sfTextSprite = uiText.m_sfTextSprite;
-	this->setText(uiText.m_sRawString);
 }
 
 void UIText::setText(const std::string& sNewText)
@@ -33,7 +18,7 @@ void UIText::setText(const std::string& sNewText)
 	m_sRawString = sNewText;
 	m_AllTexts = parseRawString();
 	this->alignTextVector();
-	UIObject::setOrigin(&m_sfTextSprite, m_sfTextSprite.getGlobalBounds().width, m_sfTextSprite.getGlobalBounds().height);
+	// Set origin called in alignTextVector
 }
 
 void UIText::handleEvent()
@@ -46,7 +31,13 @@ void UIText::update(float fFrameChunk)
 
 void UIText::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	target.draw(m_sfTextSprite);
+	for (const std::vector<sf::Text> vector : m_AllTexts)
+	{
+		for (const sf::Text text : vector)
+		{
+			target.draw(text, m_sfTextSprite.getTransform());
+		}
+	}
 }
 
 std::vector<std::vector<sf::Text>> UIText::parseRawString()
@@ -215,25 +206,5 @@ void UIText::alignTextVector()
 
 	float fTotalHeight = FontManager::getInstance().getFont(m_sFontName).getLineSpacing(30) * m_AllTexts.size(); // 30 is default char size
 
-	this->setupRenderTexture(static_cast<unsigned int>(std::ceil(fLongestLine)), static_cast<unsigned int>(std::ceil(fTotalHeight)));
-}
-
-void UIText::setupRenderTexture(unsigned int iWidth, unsigned int iHeight)
-{
-	m_sfRenderTexture->clear(sf::Color::Transparent);
-	m_sfRenderTexture->create(iWidth, iHeight);
-
-	for (const std::vector<sf::Text>& vectors : m_AllTexts)
-	{
-		for (sf::Text text : vectors)
-		{
-			// The combo of scaling and having BlendNone gives an OK effect to the RenderTexture (but messes up some fonts)
-			text.setScale(0.999f, 0.999f);
-			m_sfRenderTexture->draw(text, sf::BlendNone);
-		}
-	}
-
-	m_sfRenderTexture->display();
-
-	m_sfTextSprite.setTexture(m_sfRenderTexture->getTexture());
+	UIObject::setOrigin(&m_sfTextSprite, fLongestLine, fTotalHeight);
 }
